e02928ab2dda00ce7eebbd4cdcd9aa8fe8b7f63e
Benard <benardkhajira@gmail.com>
2024-03-31 19:06:37 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+// Define writeCards function
+function writeCards(names, eventName) {
+    let thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+        thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankYouMessages;
+}
+
+// Define countDown function
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
